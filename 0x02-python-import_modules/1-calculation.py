b07d6fe84adb0eacb5d19bbc4dd608831b90ecc0
Abrshhh <abreham.dessiem@gmail.com>
2022-10-06 23:37:26 -0700

    ads

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == __"main"__:
+    import calculator_1
+    a = 10
+    b = 5
+    print(f"{add(a, b}")
+    print(f"{sub(a, b}")
+    print(f"{mul(a, b}")
+    print(f"{div(a, b}")
