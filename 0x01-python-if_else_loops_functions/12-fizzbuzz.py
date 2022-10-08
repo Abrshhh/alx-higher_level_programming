c496eac4f0d0d691c3fda42e71f3c1d22cbaea3d
Abrshhh <abreham.dessiem@gmail.com>
2022-10-07 19:37:16 -0700

    sdef

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,4 +8,4 @@ def fizzbuzz():
         elif number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
         else:
-            print("Not Fizz or Buzz ", end="")
+            print('%d ' % (number), end="")
