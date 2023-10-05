3bcd22de76e552c8c48fe59f3bc317a4ecdb4b2d
Abrshhh <abreham.dessiem@gmail.com>
2023-10-05 11:20:04 -0700

    corrected errors

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
 
-
-def add(a, b):
     a = 1
     b = 2
     sum = a + b
     print(f"{a} + {b} = {sum}")
-
-
-add(1, 2)
