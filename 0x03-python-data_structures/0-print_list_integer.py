28e36328763506325adf0628f30adab4d82f5648
Abrshhh <abreham.dessiem@gmail.com>
2023-10-07 03:38:04 -0700

    added values

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,6 @@
 def print_list_integer(my_list=[]):
     ''' prints all integers of a list.'''
 
-    my_list = []
+    my_list = [1, 2, 3, 4, 5]
     for lists in my_list:
         print("{}".format(lists))
