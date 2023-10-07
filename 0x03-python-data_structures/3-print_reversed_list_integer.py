2f07edca522f4607a5ce8777d1c307f404b65b7b
Abrshhh <abreham.dessiem@gmail.com>
2023-10-07 13:52:05 -0700

    corrected logical errors

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,7 @@ my_list = []
 
 def print_reversed_list_integer(my_list=[]):
 
-    reverse_list = my_list.reverse()
+    my_list.reverse()
 
-    for item in reverse_list:
+    for item in my_list:
         print("{:d}".format(item))
