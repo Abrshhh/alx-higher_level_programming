e327439731fa478b6ebb99c3b49ee9045bd0aeeb
Abrshhh <abreham.dessiem@gmail.com>
2023-10-07 04:30:14 -0700

    defined my_list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
+my_list = []
 
 
 def print_list_integer(my_list=[]):
     for lists in my_list:
-        print("{}".format(lists))
+        print("{}".format(int(lists)))
