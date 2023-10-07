62ecc30fda37e8c18e084530ddd56f03c6b57785
Abrshhh <abreham.dessiem@gmail.com>
2023-10-07 05:16:44 -0700

    used isinstance

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-my_list = []
 
 
 def print_list_integer(my_list=[]):
+
+    list_entered = []
+
     for lists in my_list:
+        if isinstance(my_list, int):
+            list_entered.append(lists)
+
+    for printed_lists in list_entered:
         print("{}".format(int(lists)))
