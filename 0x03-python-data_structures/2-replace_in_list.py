12722eb9c3546111f7aa0fc0c914483b4e8983e0
Abrshhh <abreham.dessiem@gmail.com>
2023-10-08 04:17:00 -0700

    task 2 completed

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-#idx = my_list.index()
 my_list = []
 idx = my_list.index()
-#element = my_list[idx]
 
 
 def replace_in_list(my_list, idx, element):
@@ -11,4 +9,3 @@ def replace_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         my_list[idx] = my_list
         return my_list
-    
