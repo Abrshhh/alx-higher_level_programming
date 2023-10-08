9666aeed8cacfb7ce28899e93a6a71a4c134a555
Abrshhh <abreham.dessiem@gmail.com>
2023-10-08 06:01:58 -0700

    corrected the code by thinking in simplicity:)

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-my_list = []
-idx = my_list.index()
 
 
 def element_at(my_list, idx):
-    if len(my_list) == 0:
-        return None
     if idx < 0:
         return None
     elif idx >= len(my_list):
         return None
-    my_list[idx] = x
-    return x
+    if idx < len(my_list):
+        return my_list[idx]
