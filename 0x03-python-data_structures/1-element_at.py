93a096a3a5664a2258473bad4703933abc0389df
Abrshhh <abreham.dessiem@gmail.com>
2023-10-08 03:53:22 -0700

    added index() function

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,8 +4,10 @@ idx = my_list.index()
 
 
 def element_at(my_list, idx):
+    if len(my_list) == 0:
+        return None
     if idx < 0:
-        return (None)
-    elif idx > len(my_list):
-        return (None)
+        return None
+    elif idx >= len(my_list):
+        return None
     return my_list[idx]
