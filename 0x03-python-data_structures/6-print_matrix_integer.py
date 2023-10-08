227e74d50c7260a73686759669658a037b77098f
Abrshhh <abreham.dessiem@gmail.com>
2023-10-08 16:19:07 -0700

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,9 +9,9 @@ def print_matrix_integer(matrix=[[]]):
     matrix.append(row_1)
     matrix.append(row_2)
     matrix.append(row_3)
-    for item1 in row_1:
-        print("{:d}".format(item1), end="")
-    for item2 in row_2:
-        print("{:d}".format(item2), end="")
-    for item3 in row_3:
-        print("{:d}".format(item3), end="")
+    for row1 in matrix[0]:
+        print("{:d} ".format(row1), end="")
+    for row2 in matrix[1]:
+        print("\n{:d} ".format(row2), end="")
+    for row3 in matrix[2]:
+        print("\n{:d} ".format(row3), end="")
