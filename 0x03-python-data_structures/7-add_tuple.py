9cf11021e1d128be0571a3b4df942c63ecb3398c
Abrshhh <abreham.dessiem@gmail.com>
2023-10-08 17:02:02 -0700

    no need to convert the tuple into list

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,20 +4,15 @@ tuple_b = ()
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    list_a = list(tuple_a)
-    list_b = list(tuple_b)
-    if len(list_a) == 1:
-        list_a[1] = 0
-    elif len(list_a) == 0:
-        list_a[0] = 0
-        list_a[1] = 0
-    elif len(list_b) == 1:
-        list_b[1] = 0
-    elif len(list_b) == 0:
-        list_b[0] = 0
-        list_b[1] = 0
-    x = list_a[0] + list_b[0]
-    y = list_a[1] + list_b[1]
-    new_list = (x, y)
-    new_tuple = tuple(new_list)
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + ((0,) * (2 - len(tuple_a)))
+    else:
+        tuple_a = tuple_a[:2]
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + ((0,) * (2 - len(tuple_b)))
+    else:
+        tuple_b = tuple_b[:2]
+    x = tuple_a[0] + tuple_b[0]
+    y = tuple_a[1] + tuple_b[1]
+    new_tuple = (x, y)
     return new_tuple
