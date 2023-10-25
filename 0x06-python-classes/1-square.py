6cc5a0c7a3e8ef84d9ee013dce9833175bcf13f5
Abrshhh <abreham.dessiem@gmail.com>
2023-10-25 13:31:40 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''Define a class square.'''
+
+
+class Square:
+    '''This class represents a class square'''
+
+    def __init__(self, size):
+    '''Initialize a new square.
+
+    Args:
+    size(int): represents the size of the square
+    '''
+    self.__size = size
