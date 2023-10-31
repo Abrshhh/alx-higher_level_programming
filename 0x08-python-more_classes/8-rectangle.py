53c8022670b9a54eb0cb0947ff450db20b7f9688
Abrshhh <abreham.dessiem@gmail.com>
2023-10-30 17:08:47 -0700

    corrected alphabet error rect1 to rect_1

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -66,9 +66,9 @@ class Rectangle:
         Args:
             rect_1: Area of the first Rectangle.
             rect_2: Area of the second Rectangle.'''
-        if not isinstance(rect1, Rectangle):
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect2, Rectangle):
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
