fd4cddafbf0f40292f4744c98039abb528a1bf30
Abrshhh <abreham.dessiem@gmail.com>
2023-11-01 11:29:52 -0700

    task completed

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+'''Define a full name function.'''
+
+
 def say_my_name(first_name, last_name=""):
+    '''This function prints "My name is <first_name> <last_name>.
+
+    Args:
+        first_name: the first name.
+        last_name: the last name.
+
+    Raises:
+        TypeError: if first_name and last_name are not string
+    '''
+
     if not isinstance (first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance (last_name, str):
         raise TypeError("last_name must be a string")
-    print(f"My name is {first name} {last name}")
+    print(f"My name is {first name.title} {last name.title}")
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+'''Define a function printing a square with #.'''
+
+
 def print_square(size):
+    '''This function prints a square formed from "#".
+
+    Args:
+        size (int): the size length of the square.
+
+    raises:
+        TypeError: if size is not integer or size is a float and it is < 0.
+        ValueError: if size is < 0.
+    '''
+
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
