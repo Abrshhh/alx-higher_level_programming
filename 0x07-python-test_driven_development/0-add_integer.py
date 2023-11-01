6d74f4cf465023794a568aac29a100eb5a085a14
Abrshhh <abreham.dessiem@gmail.com>
2023-11-01 05:17:05 -0700

    added documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+'''Define an addition function'''
+
+
 def add_integer(a, b=98):
+    '''Returns the sum of a and b.
+
+    Float is typecasted to an integer.
+
+    Raises:
+        TyperError: if a or b is not an integer or float
+    '''
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
