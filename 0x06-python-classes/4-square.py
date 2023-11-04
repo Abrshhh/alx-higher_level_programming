f18773492f8c6ecc9fd4841daa4b296e0d1aa226
Abrshhh <abreham.dessiem@gmail.com>
2023-11-03 22:43:44 -0700

    corrected the getter and setter and added docstring

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
 
-"""Define a class Square"""
+"""Define a class Square."""
 
 
 class Square:
-    """It represents a Square"""
+    """It represents a Square."""
 
     def __init__(self, size=0):
-        """ """
+        """Initialize the class Square.
+
+        Args:
+            size: size of the square."""
         self.__size = size
-    @property 
-    def get_size(self):
-        """ """
+
+    @property
+    def size(self):
+        """Returns the size."""
         return (self.__size)
+
     @size.setter
-    def set_size(self, value):
-        """ """
+    def size(self, value):
+        """Set new size to the class Square.
+
+        Args:
+            value: new size of the Square."""
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
         return value
-    def area(self):
-        """ """
+
+   def area(self):
+        """Calculates the area of the square."""
         return (self.__size * self.__size)
