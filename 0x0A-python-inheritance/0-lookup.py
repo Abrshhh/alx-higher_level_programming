e82ad066fbcac605cdba5618fb8e4957c5fae6c0
Abrshhh <abreham.dessiem@gmail.com>
2023-11-06 12:44:57 -0800

    task 0 completed

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Define a function returning attribute and method.'''
+
+def lookup(obj):
+    ''' Returns list of attribute and methods of obj.
+
+    Args:
+        obj: any object
+    '''
+
+    return obj.__dir__()
