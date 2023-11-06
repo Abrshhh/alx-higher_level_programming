25f753f1b6ad9daa471cb3e4723c91765a5ab65b
Abrshhh <abreham.dessiem@gmail.com>
2023-11-06 15:14:33 -0800

    added whitespace

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-'''Define a function.'''
+"""Defines an inherited class-checking function."""
 
 
 def inherits_from(obj, a_class):
-    '''This function specifies if an object is an instance of a class.
+    """Checks if an object is an inherited instance of a class.
 
     Args:
-        obj (any): any object.
-        a_class (type): the specified class.
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
     Returns:
-        True: if the object is an instance of the class inherited (directly or indirectly) from the specified class.
-        False: if not.
-    '''
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
