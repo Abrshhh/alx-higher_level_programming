563c7017a021214272a7c789614af22dc3301ad4
Abrshhh <abreham.dessiem@gmail.com>
2023-11-06 13:50:12 -0800

    task 3 completed

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Define a function.'''
+
+def is_kind_of_class(obj, a_class):
+    '''This function specifies if the object is an instance of,
+    or if the object is an instance of a class that inherited from, the specified class.
+
+    Args:
+        obj: the object.
+        a_class: the specified class.
+
+    Returns:
+        True or False if the object fullfuill the requirement or not
+    '''
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
