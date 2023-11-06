12f03d4b3bef4ede180961d3e12f5ce90ad3c718
Abrshhh <abreham.dessiem@gmail.com>
2023-11-06 14:58:03 -0800

    added whitespace

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-'''Define a function.'''
+"""Defines a class and inherited class-checking function."""
 
 
 def is_kind_of_class(obj, a_class):
-    '''This function specifies if the object is an instance of,
-    or if the object is an instance of a class that inherited from, the specified class.
+    """Check if an object is an instance or inherited instance of a class.
 
     Args:
-        obj: the object.
-        a_class: the specified class.
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
 
     Returns:
-        True or False if the object fullfuill the requirement or not
-    '''
+        If obj is an instance or inherited instance of a_class - True.
+        Otherwise - False.
+    """
     if isinstance(obj, a_class):
         return True
-    else:
-        return False
+    return False
