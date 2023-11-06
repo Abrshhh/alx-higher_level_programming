255863cf1c2650aca1c683a639df5c91283124b9
Abrshhh <abreham.dessiem@gmail.com>
2023-11-06 15:05:37 -0800

    added whitespace

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,7 +8,6 @@ def inherits_from(obj, a_class):
     Args:
         obj: any object
         a_class: the specified class
-
     Returns:
         True: if the object is an instance of the class inherited (directly or indirectly) from the specified class or
         False: if not
