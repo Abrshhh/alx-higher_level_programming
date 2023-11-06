535060d7b6c7bee7c1a5c8ff000a1fb6e2fab8ef
Abrshhh <abreham.dessiem@gmail.com>
2023-11-06 15:02:34 -0800

    added whitespace and change code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''Define a function.'''
 
+
 def inherits_from(obj, a_class):
     '''This function specifies if an object is an instance of a class
-    that inherited (directly or indirectly) from the specified class.
 
     Args:
         obj: any object
@@ -14,7 +14,7 @@ def inherits_from(obj, a_class):
         False: if not
     '''
 
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class
         return True
     else:
         return False
