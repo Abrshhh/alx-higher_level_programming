4c77898c2a5a62444450e01ce7d813a2224b8154
Abrshhh <abreham.dessiem@gmail.com>
2023-11-06 13:31:12 -0800

    task 2 completed

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Define a function.'''
+
+def is_same_class(obj, a_class):
+    '''This function specifies if an object is an instance of a class or not
+
+    Args:
+        obj: any object
+        a_class: the specified class
+
+    Returns:
+        True: if the object is an instance of the class
+        False: if the object is not an instance of the class
+    '''
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
