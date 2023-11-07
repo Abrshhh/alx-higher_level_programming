3f4be1974ea60208088182e84da8224ddcfb66bb
Abrshhh <abreham.dessiem@gmail.com>
2023-11-07 12:56:20 -0800

    removed the single quotation from filename

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     '''Reads and prints a text file.'''
 
-    with open('filename', 'r', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
