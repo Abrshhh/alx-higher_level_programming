f7ada249a978e3edcb2fea1468852de11b4e9b3a
Abrshhh <abreham.dessiem@gmail.com>
2023-11-07 13:28:00 -0800

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,14 @@
 
 
 def write_file(filename="", text=""):
-    '''Returns the number of character written.'''
+    '''Returns the number of character written.
+
+
+    Args:
+        filename (str): the name of the file.
+        text (str): the text to be written.
+
+    Returns: the number of character written.'''
 
     with open(filename, 'w', encoding="utf-8") as f:
         return (f.write(text), end="")
