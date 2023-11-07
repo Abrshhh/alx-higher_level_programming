745de9491c68b503a771e7846b15e8ab53b5ee42
Abrshhh <abreham.dessiem@gmail.com>
2023-11-07 12:49:52 -0800

    task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Define a text reading function.'''
+
+
+def read_file(filename=""):
+    '''Reads and prints a text file.'''
+
+    with open('filename', 'r', encoding="utf-8") as f:
+        for line in f:
+            print(line)
