947f70eb32cbb2a9b58b6bda708b764500b184b0
Abrshhh <abreham.dessiem@gmail.com>
2023-11-07 12:54:14 -0800

    added .read()

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,4 @@ def read_file(filename=""):
     '''Reads and prints a text file.'''
 
     with open('filename', 'r', encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.read(), end="")
