26d2516e2d4b5f1f792eccd96d934e1bf9722abf
Abrshhh <abreham.dessiem@gmail.com>
2023-11-08 03:01:16 -0800

    task 3 completed

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Define a JSON representing function.'''
+
+
+import json
+def to_json_string(my_obj):
+    '''Gives JSON representation of an object.
+
+    Args:
+        my_obj (str): the object to be converted
+
+    Returns: JSON representation
+    '''
+
+    return json.dumps(my_obj)
