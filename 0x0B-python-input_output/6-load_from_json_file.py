9657251faed32660bf4c95add6e6164b37c013f7
Abrshhh <abreham.dessiem@gmail.com>
2023-11-08 11:37:14 -0800

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''Define a text creating function.'''
-
-
 import json
+
+
 def load_from_json_file(filename):
     '''Create an object from JSON file.'''
 
     with open(filename) as f:
-        f.filename
+        json.load(f)
