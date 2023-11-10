5ebe48a08600351218e312d4383810c0e7ffa83c
Abrshhh <abreham.dessiem@gmail.com>
2023-11-10 13:25:02 -0800

    task 2 completed

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+'''Define a class called Rextangle.'''
+
+
+class Rectangle(Base):
+    '''Represents a Rectangle that inherits from the class Base.'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''Initialize the class Rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): an integer.
+            y (int): an integer.
+            id (int): an integer.
+        '''
+
+        super().__init__(id) #Call the __init__method of the class Base.
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def get_width(self):
+        '''Return the width of the Rectangle.'''
+        return self.__width
+    @get_width.setter
+    def set_width(self, value):
+        '''Set the width of the Rectangle.'''
+        self.__width = value
+
+    @property
+    def get_height(self):
+        '''Return the height of the Rectangle.'''
+        return self.__height
+    @get_height.setter
+    def set_height(self, value):
+        '''Set the height of the Rectangle.'''
+        self.__height = value
+
+    @property
+    def get_x(self):
+        '''Return the value of x.'''
+        return self.__x
+    @get_x.setter
+    def set_x(self, value):
+        '''Set the value of x.'''
+        self.__x = value
+
+    @property
+    def get_y(self):
+        '''Get the value of y.'''
+        return self.__y
+    @get_y.setter
+    def set_y(self, value):
+        '''Set the value of y.'''
+        self.__y = value
