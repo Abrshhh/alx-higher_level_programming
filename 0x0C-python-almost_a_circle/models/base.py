c966dfc6631246134a865929f4690bbb267f91e7
Abrshhh <abreham.dessiem@gmail.com>
2023-11-12 13:53:05 -0800

    added Json package

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''Define a class called Base.'''
+import json
 
 
 class Base:
@@ -14,3 +15,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Gives JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): The list of dictionaries converted to JSON string.
+        Returns:
+            the string "[]" : if list_dictionaries is None or empty.
+            JSON string (str): if list_dictionaries is not empty or None.
+        '''
+
+    if len(list_dictionaries) == 0 or list_dictionaries = None:
+        return ("[]")
+    else:
+        return json.dumps(list_dictionaries)
