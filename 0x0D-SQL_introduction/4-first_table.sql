a0233518c08d4185141312cb3e138c074ceef252
Abrshhh <abreham.dessiem@gmail.com>
2023-12-12 09:16:22 -0800

    added comments

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,10 @@
 -- creates a table called first_table in the current database in your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
+-- switch to the database
 USE hbtn_0c_0;
 
+-- create the table
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256)
