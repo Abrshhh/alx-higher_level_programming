4daab39a09d50654a720c3a4147372b9b5f0698f
Abrshhh <abreham.dessiem@gmail.com>
2023-12-12 10:29:02 -0800

    added backtick

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,11 +1,5 @@
 -- creates a table called first_table in the current database in your MySQL server.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- switch to the database
-USE hbtn_0c_0;
-
--- create the table
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT,
-	name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `first_table` (
+	`id` INT,
+	`name` VARCHAR(256)
 );
